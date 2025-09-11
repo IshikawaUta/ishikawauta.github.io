2a0c079bb5c6d1ddd63c0934b17a5f4e3fe9acf9
Eka saputra <jayaj7129@gmail.com>
2025-09-11 14:04:32 +0000

    belum berubah (_for element <LinkComponent>_)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
             <Button asChild size="lg" className="btn-slide-effect">
                 <Link href="/contact">LET'S START</Link>
             </Button>
-            <Button asChild size="lg" variant="outline" className="btn-slide-effect-inverted">
+            <Button asChild size="lg" className="btn-slide-effect-inverted">
                 <Link href="/portfolio">My Portfolio</Link>
             </Button>
           </div>
