5d4cb056a3eb94c75894baa480183488f7bd782a
Eka saputra <jayaj7129@gmail.com>
2025-09-11 13:31:12 +0000

    buat seperti ini

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ export default function Home() {
             Based in Tangerang, Indonesia.
           </p>
           <div className="flex gap-4 justify-center">
-            <Button asChild size="lg">
+            <Button asChild size="lg" className="btn-custom">
                 <Link href="/contact">LET'S START</Link>
             </Button>
             <Button asChild size="lg" variant="outline">
