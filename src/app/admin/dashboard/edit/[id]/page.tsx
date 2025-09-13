ef08c92cd63ba8250c8b2ee9a43957f9cbd51959
Eka saputra <jayaj7129@gmail.com>
2025-09-13 05:12:31 +0000

    Try fixing this error: `Console Error: A param property was accessed dir

--- src/app/admin/dashboard/edit/[id]/page.tsx
+++ src/app/admin/dashboard/edit/[id]/page.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import { ProjectForm } from "../../project-form";
@@ -5,9 +6,8 @@ import { notFound } from 'next/navigation';
 import { useProjects } from "../../project-context";
 
 export default function EditProjectPage({ params }: { params: { id: string } }) {
-  const { id } = params;
   const { projects } = useProjects();
-  const projectId = parseInt(id, 10);
+  const projectId = parseInt(params.id, 10);
   
   const project = projects.find((p) => p.id === projectId);
 
